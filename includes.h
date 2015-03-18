20eb42c1c90a6cac0ebfa9ce78f0a936f43706bf
Johannes Lier <johannes.o.lier@gmail.com>
2015-03-18 16:37:01 -0700

    Johannes' include header

---
+++ includes.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cmath>
+
+#include <GL\glut.h>
+
+#include <opencv\cv.h>
+#include <opencv2\highgui\highgui.hpp>
+
+#include <cstdio>
